550cdc597843c5689850d86dc7b90f8fdaf32ee5
MrLikviduk <v@086.kz>
2018-08-06 10:39:26 +0600

    Three tasks are solved!

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -2,6 +2,8 @@
 namespace MrLikviduk\Registration\Console;
 
 use App\Console\Kernel as ConsoleKernel;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Mail;
 use MrLikviduk\Registration\Mail\CheckNewUsersMailMail;
@@ -18,9 +20,13 @@ class Kernel extends ConsoleKernel
     {
         parent::schedule($schedule);
 
+        $periodicity = 90; // Периодичность проверки (в минутах)
+
         $schedule->call(function () {
-            Mail::queue(new CheckNewUsersMailMail(rand(1, 50), 60));
-        })->hourly();
+            $periodicity = 90; // Периодичность проверки (в минутах)
+            $users_count = User::where('created_at', '>=', Carbon::now()->subMinutes($periodicity))->count();
+            Mail::queue(new CheckNewUsersMailMail($users_count, $periodicity));
+        })->cron('*/'.$periodicity.' * * * *');
 
     }
 }
